f8bcd7f3d1ee3b06d40894d6fb722c417c69375d
Francesc Alted <faltet@gmail.com>
2018-09-20 09:27:32 +0200

    Cleanup

--- iarray/iarray.c
+++ iarray/iarray.c
@@ -28,15 +28,16 @@ const float GB = 1024 * MB;
 
 const int NCHUNKS = 500;
 const int CHUNKSIZE = 200 * 1000;  // fits well in modern L3 caches
+const int NELEM = NCHUNKS * CHUNKSIZE;  // multiple of CHUNKSIZE for now
 const int NTHREADS = 4;
 
 
 // Fill X values in regular array
 int fill_x(double *x) {
-  double incx = 10. / (NCHUNKS * CHUNKSIZE);
+  double incx = 10. / NELEM;
 
   /* Fill even values between 0 and 10 */
-  for (int i = 0; i < NCHUNKS * CHUNKSIZE; i++) {
+  for (int i = 0; i < NELEM; i++) {
     x[i] = incx * i;
   }
   return 0;
@@ -44,7 +45,7 @@ int fill_x(double *x) {
 
 // Compute and fill X values in a buffer
 void fill_buffer(double *x, int nchunk) {
-  double incx = 10. / (NCHUNKS * CHUNKSIZE);
+  double incx = 10. / NELEM;
 
   for (int i = 0; i < CHUNKSIZE; i++) {
     x[i] = incx * (nchunk * CHUNKSIZE + i);
@@ -67,7 +68,7 @@ double poly(const double x) {
 
 // Compute and fill Y values in regular array
 void compute_y(const double *x, double *y) {
-  for (int i = 0; i < NCHUNKS * CHUNKSIZE; i++) {
+  for (int i = 0; i < NELEM; i++) {
     y[i] = poly(x[i]);
   }
 }
@@ -79,25 +80,13 @@ void fill_buffer_y(const double *x, double *y) {
   }
 }
 
-void find_root(const double *x, const double *y,
-               const double prev_value) {
-  double pv = prev_value;
-  int last_root_idx = -1;
 
-  for (int i = 0; i < CHUNKSIZE; i++) {
-    double yi = y[i];
-    if (((yi > 0) - (yi < 0)) != ((pv > 0) - (pv < 0))) {
-      if (last_root_idx != (i - 1)) {
-        printf("%.16g, ", x[i]);
-        last_root_idx = i;  // avoid the last point (ULP effects)
-      }
-    }
-    pv = yi;
-  }
-}
+int main() {
+  printf("Blosc version info: %s (%s)\n",
+         BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
 
+  blosc_init();
 
-int compute_vectors(void) {
   const size_t isize = CHUNKSIZE * sizeof(double);
   double buffer_x[CHUNKSIZE];
   double buffer_y[CHUNKSIZE];
@@ -120,7 +109,7 @@ int compute_vectors(void) {
   dparams.nthreads = NTHREADS;
 
   // Fill the plain x operand
-  static double x[NCHUNKS * CHUNKSIZE];
+  static double x[NELEM];
   blosc_set_timestamp(&last);
   fill_x(x);
   blosc_set_timestamp(&current);
@@ -141,18 +130,20 @@ int compute_vectors(void) {
          (1. * sc_x->nbytes) / sc_x->cbytes);
 
   // Compute the plain y vector
-  static double y[NCHUNKS * CHUNKSIZE];
+  static double y[NELEM];
   blosc_set_timestamp(&last);
   compute_y(x, y);
   blosc_set_timestamp(&current);
   ttotal = blosc_elapsed_secs(last, current);
   printf("Time for computing and filling Y values: %.3g s, %.1f MB/s\n",
-         ttotal, sizeof(x) / (ttotal * MB));
+         ttotal, sizeof(y) / (ttotal * MB));
+  // To prevent the optimizer to be too smart and remove 'dead' code
+  int retcode = y[0] > y[1];
 
   // Create a super-chunk container and compute y values
   sc_y = blosc2_new_schunk(cparams, dparams, NULL);
   blosc_set_timestamp(&last);
-  for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
+  for (nchunk = 0; nchunk < sc_x->nchunks; nchunk++) {
     dsize = blosc2_schunk_decompress_chunk(sc_x, nchunk, buffer_x, isize);
     if (dsize < 0) {
       printf("Decompression error.  Error code: %d\n", dsize);
@@ -169,60 +160,12 @@ int compute_vectors(void) {
          sc_y->nbytes / MB, sc_y->cbytes / MB,
          (1. * sc_y->nbytes) / sc_y->cbytes);
 
-  // Find the roots of the polynomial
-  printf("Roots found at: ");
-  blosc_set_timestamp(&last);
-  prev_value = y[0];
-  for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
-    find_root(x + nchunk * CHUNKSIZE, y + nchunk * CHUNKSIZE, prev_value);
-    prev_value = y[(nchunk + 1) * CHUNKSIZE - 1];
-  }
-  blosc_set_timestamp(&current);
-  ttotal = blosc_elapsed_secs(last, current);
-  printf("\n");
-  printf("Find root time:  %.3g s, %.1f MB/s\n",
-         ttotal, 2. * sizeof(x) / (ttotal * MB));    // 2 super-chunks involved
-
-  // Find the roots of the polynomial
-  printf("Roots found at: ");
-  blosc_set_timestamp(&last);
-  prev_value = buffer_y[0];
-  for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
-    dsize = blosc2_schunk_decompress_chunk(sc_y, nchunk, (void *) buffer_y, isize);
-    if (dsize < 0) {
-      printf("Decompression error.  Error code: %d\n", dsize);
-      return dsize;
-    }
-    dsize = blosc2_schunk_decompress_chunk(sc_x, nchunk, (void *) buffer_x, isize);
-    if (dsize < 0) {
-      printf("Decompression error.  Error code: %d\n", dsize);
-      return dsize;
-    }
-    find_root(buffer_x, buffer_y, prev_value);
-    prev_value = buffer_y[CHUNKSIZE - 1];
-  }
-  blosc_set_timestamp(&current);
-  ttotal = blosc_elapsed_secs(last, current);
-  printf("\n");
-  printf("Find root time (compressed):  %.3g s, %.1f MB/s\n",
-         ttotal, (sc_x->nbytes + sc_y->nbytes) / (ttotal * MB));    // 2 super-chunks involved
 
   // Free resources
   blosc2_free_schunk(sc_x);
   blosc2_free_schunk(sc_y);
-  return 0;
-}
-
-
-int main() {
-  printf("Blosc version info: %s (%s)\n",
-         BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
-
-  blosc_init();
-
-  compute_vectors();
 
   blosc_destroy();
 
-  return 0;
+  return retcode;
 }
