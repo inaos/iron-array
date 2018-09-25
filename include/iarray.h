4f8c38e8347e916004ca0a2f424cf299bed60fb1
Christian Steiner <christian.steiner@inaos.ch>
2018-09-25 09:10:03 +0200

    progress implementing api

--- include/iarray.h
+++ include/iarray.h
@@ -15,7 +15,6 @@ typedef struct iarray_expression_s iarray_expression_t;
 
 typedef struct iarray_config_s {
 	void *cparams;
-
 } iarray_config_t;
 
 typedef enum iarray_rng_e {
@@ -40,19 +39,19 @@ typedef struct iarray_slice_param_s {
 } iarray_slice_param_t;
 
 INA_API(ina_rc_t) iarray_ctx_new(iarray_config_t *cfg, iarray_context_t **ctx);
-INA_API(ina_rc_t) iarray_ctx_free(iarray_context_t **ctx);
+INA_API(void) iarray_ctx_free(iarray_context_t **ctx);
 
-INA_API(ina_rc_t) iarray_arange(iarray_context_t *ctx, int start, int stop, int step, iarray_data_type_t dtype, iarray_container_t **container);
+INA_API(ina_rc_t) iarray_arange(iarray_context_t *ctx, iarray_dtshape_t *dtshape, int start, int stop, int step, iarray_data_type_t dtype, iarray_container_t **container);
 INA_API(ina_rc_t) iarray_zeros(iarray_context_t *ctx, iarray_dtshape_t *dtshape, iarray_data_type_t dtype, iarray_container_t **container);
 INA_API(ina_rc_t) iarray_ones(iarray_context_t *ctx, iarray_dtshape_t *dtshape, iarray_data_type_t dtype, iarray_container_t **container);
 INA_API(ina_rc_t) iarray_fill_float(iarray_context_t *ctx, iarray_dtshape_t *dtshape, float value, iarray_container_t **container);
 INA_API(ina_rc_t) iarray_fill_double(iarray_context_t *ctx, iarray_dtshape_t *dtshape, double value, iarray_container_t **container);
-INA_API(ina_rc_t) iarray_rand(iarray_context_t *ctx, int shape[], iarray_rng_t rng, iarray_data_type_t dtype, iarray_container_t **container);
+INA_API(ina_rc_t) iarray_rand(iarray_context_t *ctx, iarray_dtshape_t *dtshape, iarray_rng_t rng, iarray_data_type_t dtype, iarray_container_t **container);
 
 INA_API(ina_rc_t) iarray_slice(iarray_context_t *ctx, iarray_container_t *c, iarray_slice_param_t *params, iarray_container_t **container);
 
 INA_API(ina_rc_t) iarray_expr_new(iarray_context_t *ctx, iarray_expression_t **e);
-INA_API(ina_rc_t) iarray_expr_free(iarray_context_t *ctx, iarray_expression_t **e);
+INA_API(void) iarray_expr_free(iarray_context_t *ctx, iarray_expression_t **e);
 INA_API(ina_rc_t) iarray_expr_bind(iarray_expression_t *e, const char *var, iarray_container_t *val);
 INA_API(ina_rc_t) iarray_expr_bind_scalar_float(iarray_expression_t *e, const char *var, float val);
 INA_API(ina_rc_t) iarray_expr_bind_scalar_double(iarray_expression_t *e, const char *var, double val);
