fd4d1907288340dc1e1cac4f2ec764d7d29ee9c1
aleix11alcacer <al312972@uji.es>
2018-11-07 11:22:40 +0100

    newline added at the end of file

--- tests/main.c
+++ tests/main.c
@@ -3,4 +3,6 @@
 //
 
 #include <libinac/lib.h>
-int main(int argc,  char** argv) { return ina_test_run(argc, argv, NULL);}
+int main(int argc,  char** argv) {
+    return ina_test_run(argc, argv, NULL);
+}
