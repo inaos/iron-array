b80013f7c624afc009af525e6e7b178eda0d1d52
Francesc Alted <faltet@gmail.com>
2018-11-07 12:14:30 +0100

    test_eval.c is working now (on top of caterva containers)

--- tests/test_eval.c
+++ tests/test_eval.c
@@ -21,6 +21,9 @@
 #define MB  (1024*KB)
 #define GB  (1024*MB)
 
+//static double vector_x[NELEM];
+//static double vector_y[NELEM];
+
 /* Compute and fill X values in a buffer */
 void fill_buffer(double* x, int nchunk, int nitems)
 {
@@ -63,7 +66,15 @@ int test_schunks_equal_double(blosc2_schunk* sc1, blosc2_schunk* sc2)
     double *buffer_sc2 = malloc(chunksize);
     for (int nchunk = 0; nchunk < sc1->nchunks; nchunk++) {
         int dsize = blosc2_schunk_decompress_chunk(sc1, nchunk, buffer_sc1, chunksize);
+        if (dsize < 0) {
+            fprintf(stderr, "Error in decompressing a chunk from sc1\n");
+            return false;
+        }
         dsize = blosc2_schunk_decompress_chunk(sc2, nchunk, buffer_sc2, chunksize);
+        if (dsize < 0) {
+            fprintf(stderr, "Error in decompressing a chunk from sc2\n");
+            return false;
+        }
         for (int nelem = 0; nelem < nitems_in_chunk; nelem++) {
             double vdiff = fabs(buffer_sc1[nelem] - buffer_sc2[nelem]);
             if (vdiff > 1e-6) {
@@ -82,9 +93,9 @@ int test_schunks_equal_double(blosc2_schunk* sc1, blosc2_schunk* sc2)
 INA_TEST_DATA(eval)
 {
     int tests_run;
-    blosc2_schunk *sc_x;
-    blosc2_schunk *sc_y;
-    blosc2_schunk *sc_out;
+    caterva_array *cta_x;
+    caterva_array *cta_y;
+    caterva_array *cta_out;
     int nbytes;
     int cbytes;
     int clevel;
@@ -94,8 +105,6 @@ INA_TEST_DATA(eval)
 
 INA_TEST_SETUP(eval)
 {
-    const size_t isize = NITEMS_CHUNK * sizeof(double);
-
     blosc_init();
 
     // Create a super-chunk container for input (X values)
@@ -113,31 +122,43 @@ INA_TEST_SETUP(eval)
     data->buffer_x = ina_mem_alloc(sizeof(double)*NITEMS_CHUNK);
     data->buffer_y = ina_mem_alloc(sizeof(double)*NITEMS_CHUNK);
 
-    data->sc_x = blosc2_new_schunk(cparams, dparams, NULL);
-    fill_sc_x(data->buffer_x, data->sc_x);
+    // Create and fill the caterva container for x values
+    blosc2_frame frame_x = BLOSC_EMPTY_FRAME;
+    caterva_pparams pparams = CATERVA_PPARAMS_ONES;
+    pparams.shape[CATERVA_MAXDIM - 1] = NELEM;
+    pparams.cshape[CATERVA_MAXDIM - 1] = NITEMS_CHUNK;
+    caterva_array *cta_x = caterva_new_array(cparams, dparams, &frame_x, pparams);
+    data->cta_x = cta_x;
+    fill_sc_x(data->buffer_x, cta_x->sc);
 
     // Create a super-chunk container for output (Y values)
-    data->sc_y = blosc2_new_schunk(cparams, dparams, NULL);
-    for (int nchunk = 0; nchunk < data->sc_x->nchunks; nchunk++) {
-        int dsize = blosc2_schunk_decompress_chunk(data->sc_x, nchunk, data->buffer_x, isize);
-        if (dsize<0) {
+    const size_t isize = NITEMS_CHUNK * sizeof(double);
+    blosc2_frame frame_y = BLOSC_EMPTY_FRAME;
+    caterva_array *cta_y = caterva_new_array(cparams, dparams, &frame_y, pparams);
+    data->cta_y = cta_y;
+    blosc2_schunk *sc_x = cta_x->sc, *sc_y = cta_y->sc;
+    for (int nchunk = 0; nchunk < sc_x->nchunks; nchunk++) {
+        int dsize = blosc2_schunk_decompress_chunk(sc_x, nchunk, data->buffer_x, isize);
+        if (dsize < 0) {
             INA_TEST_MSG("Decompression error.  Error code: %d\n", dsize);
             INA_TEST_ASSERT_TRUE(0);
         }
         int nitems = (nchunk < NCHUNKS - 1) ? NITEMS_CHUNK : NELEM - nchunk * NITEMS_CHUNK;
         fill_buffer_y(data->buffer_x, data->buffer_y, nitems);
-        blosc2_schunk_append_buffer(data->sc_y, data->buffer_y, nitems * sizeof(double));
+        blosc2_schunk_append_buffer(sc_y, data->buffer_y, nitems * sizeof(double));
     }
 
-    // Create a super-chunk container for eval output (OUT values)
-    data->sc_out = blosc2_new_schunk(cparams, dparams, NULL);
+    // Create a caterva container for eval output (OUT values)
+    blosc2_frame frame_out = BLOSC_EMPTY_FRAME;
+    caterva_array *cta_out = caterva_new_array(cparams, dparams, &frame_out, pparams);
+    data->cta_out = cta_out;
 }
 
 INA_TEST_TEARDOWN(eval)
 {
-    blosc2_free_schunk(data->sc_x);
-    blosc2_free_schunk(data->sc_y);
-    blosc2_free_schunk(data->sc_out);
+    caterva_free_array(data->cta_x);
+    caterva_free_array(data->cta_y);
+    caterva_free_array(data->cta_out);
     ina_mem_free(data->buffer_x);
     ina_mem_free(data->buffer_y);
 }
@@ -145,20 +166,20 @@ INA_TEST_TEARDOWN(eval)
 INA_TEST_FIXTURE(eval, chunk1)
 {
     iarray_context_t *iactx;
-    iarray_config_t cfg = { .max_num_threads = NTHREADS,.flags = IARRAY_EXPR_EVAL_CHUNK };
+    iarray_config_t cfg = { .max_num_threads = NTHREADS, .flags = IARRAY_EXPR_EVAL_CHUNK };
     iarray_ctx_new(&cfg, &iactx);
     iarray_expression_t* e;
     iarray_expr_new(iactx, &e);
     iarray_container_t* c_x;
-    iarray_from_sc(iactx, data->sc_x, IARRAY_DATA_TYPE_DOUBLE, &c_x);
+    iarray_from_ctarray(iactx, data->cta_x, IARRAY_DATA_TYPE_DOUBLE, &c_x);
     iarray_expr_bind(e, "x", c_x);
     iarray_container_t* c_out;
-    iarray_from_sc(iactx, data->sc_out, IARRAY_DATA_TYPE_DOUBLE, &c_out);
+    iarray_from_ctarray(iactx, data->cta_out, IARRAY_DATA_TYPE_DOUBLE, &c_out);
 
     iarray_expr_compile(e, "(x - 1.35) * (x - 4.45) * (x - 8.5)");
     iarray_eval(e, c_out);
     
-    INA_TEST_ASSERT_TRUE(test_schunks_equal_double(data->sc_y, data->sc_out));
+    INA_TEST_ASSERT_TRUE(test_schunks_equal_double(data->cta_y->sc, data->cta_out->sc));
 
     iarray_expr_free(iactx, &e);
     iarray_ctx_free(&iactx);
@@ -172,15 +193,15 @@ INA_TEST_FIXTURE(eval, block1)
     iarray_expression_t* e;
     iarray_expr_new(iactx, &e);
     iarray_container_t* c_x;
-    iarray_from_sc(iactx, data->sc_x, IARRAY_DATA_TYPE_DOUBLE, &c_x);
+    iarray_from_ctarray(iactx, data->cta_x, IARRAY_DATA_TYPE_DOUBLE, &c_x);
     iarray_expr_bind(e, "x", c_x);
     iarray_container_t* c_out;
-    iarray_from_sc(iactx, data->sc_out, IARRAY_DATA_TYPE_DOUBLE, &c_out);
+    iarray_from_ctarray(iactx, data->cta_out, IARRAY_DATA_TYPE_DOUBLE, &c_out);
 
     iarray_expr_compile(e, "(x - 1.35) * (x - 4.45) * (x - 8.5)");
     iarray_eval(e, c_out);
     
-    INA_TEST_ASSERT_TRUE(test_schunks_equal_double(data->sc_y, data->sc_out));
+    INA_TEST_ASSERT_TRUE(test_schunks_equal_double(data->cta_y->sc, data->cta_out->sc));
 
     iarray_expr_free(iactx, &e);
     iarray_ctx_free(&iactx);
