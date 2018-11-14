55cd997a8d42cedba5f8b82f542f44ccc5b026fc
Christian A. Steiner <christian.steiner@inaos.ch>
2018-11-14 12:20:46 +0100

    progress

--- tests/test_gemm.c
+++ tests/test_gemm.c
@@ -1,14 +1,14 @@
 /*
-* Copyright INAOS GmbH, Thalwil, 2018.
-* Copyright Francesc Alted, 2018.
-*
-* All rights reserved.
-*
-* This software is the confidential and proprietary information of INAOS GmbH
-* and Francesc Alted ("Confidential Information"). You shall not disclose such Confidential
-* Information and shall use it only in accordance with the terms of the license agreement.
-*
-*/
+ * Copyright INAOS GmbH, Thalwil, 2018.
+ * Copyright Francesc Alted, 2018.
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of INAOS GmbH
+ * and Francesc Alted ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the license agreement.
+ *
+ */
 
 #include <libiarray/iarray.h>
 #include <iarray_private.h>
