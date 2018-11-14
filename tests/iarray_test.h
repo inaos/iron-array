767c227db89d66fa5b22083a7c8c5795f9843dbf
Christian A. Steiner <christian.steiner@inaos.ch>
2018-11-14 12:20:46 +0100

    progress

--- tests/iarray_test.h
+++ tests/iarray_test.h
@@ -1,14 +1,14 @@
 /*
-* Copyright INAOS GmbH, Thalwil, 2018.
-* Copyright Francesc Alted, 2018.
-*
-* All rights reserved.
-*
-* This software is the confidential and proprietary information of INAOS GmbH
-* and Francesc Alted ("Confidential Information"). You shall not disclose such Confidential
-* Information and shall use it only in accordance with the terms of the license agreement.
-*
-*/
+ * Copyright INAOS GmbH, Thalwil, 2018.
+ * Copyright Francesc Alted, 2018.
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of INAOS GmbH
+ * and Francesc Alted ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the license agreement.
+ *
+ */
 
 #ifndef _IARRAY_TEST_COMMON_H_
 #define _IARRAY_TEST_COMMON_H_
