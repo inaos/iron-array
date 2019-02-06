f74aef45967d6243aaab9ad3a9c44a3bb71b52b2
Christian Steiner <christian.steiner@inaos.ch>
2019-02-06 16:16:53 +0100

    fixed error and perf tool specific stuff

--- bench/bench_matmul.c
+++ bench/bench_matmul.c
@@ -13,17 +13,12 @@
 #include <libiarray/iarray.h>
 #include <iarray_private.h>
 
-#define NELEM_BYTES(nelem) (nelem * sizeof(double))
 #define NTHREADS 1
+#define NELEM_BYTES(nelem) (nelem * sizeof(double))
 
 INA_BENCH_DATA(matmul) {
     iarray_context_t *ctx;
     iarray_config_t config;
-    ina_stopwatch_t *w;
-
-    const char *mat_x_name;
-    const char *mat_y_name;
-    const char *mat_out_name;
 
     uint64_t nbytes;
     uint64_t cbytes;
@@ -63,17 +58,12 @@ INA_BENCH_DATA(matmul) {
     uint64_t flops;
 
     int flags;
-    iarray_store_properties_t *mat_x_prop;
-    iarray_store_properties_t *mat_y_prop;
-    iarray_store_properties_t *mat_out_prop;
 
     iarray_container_t *con_x;
     iarray_container_t *con_y;
 
     double *mat_x;
     double *mat_y;
-    double *mat_out;
-    double *mat_res;
 };
 
 static ina_rc_t _init_message(struct matmul_data *data)
@@ -118,7 +108,11 @@ static ina_rc_t _set_matrix_config(struct matmul_data *data,
     data->shape_y_1 = shape_y_1;
 
     data->pshape_y_0 = bshape_y_0;
-    data->bshape_y_1 = bshape_y_1;
+    data->pshape_y_1 = bshape_y_1;
+
+    data->bshape_y_0 = pshape_y_0;
+    data->bshape_y_1 = pshape_y_1;
+
     data->size_y = shape_y_0 * shape_y_1;
 
     data->shape_out_0 = shape_x_0;
@@ -132,23 +126,17 @@ static ina_rc_t _set_matrix_config(struct matmul_data *data,
     data->flops = (2 * data->shape_x_1 - 1) * data->shape_x_0 * data->shape_y_1;
 
     data->flags = 0;
-    data->mat_x_prop = NULL;
-    data->mat_y_prop = NULL;
-    data->mat_out_prop = NULL;
 
     return INA_SUCCESS;
 }
 
-static ina_rc_t _setup_matrices(struct matmul_data *data, int allocated)
+static ina_rc_t _setup_matrices(struct matmul_data *data)
 {
-    double elapsed_sec = 0;
-
     INA_MUST_SUCCEED(iarray_context_new(&data->config, &data->ctx));
 
-    data->mat_x = (double *)ina_mem_alloc((sizeof(double) * data->size_x));
-    data->mat_y = (double *)ina_mem_alloc((sizeof(double) * data->size_y));
+    data->mat_x = (double*)ina_mem_alloc((sizeof(double) * data->size_x));
+    data->mat_y = (double*)ina_mem_alloc((sizeof(double) * data->size_y));
 
-    INA_STOPWATCH_START(data->w);
     double incx = 10. / data->size_x;
     for (uint64_t i = 0; i < data->size_x; i++) {
         data->mat_x[i] = i * incx;
@@ -157,51 +145,28 @@ static ina_rc_t _setup_matrices(struct matmul_data *data, int allocated)
     for (uint64_t i = 0; i < data->size_y; i++) {
         data->mat_y[i] = i * incy;
     }
-    INA_STOPWATCH_STOP(data->w);
-
-    INA_MUST_SUCCEED(ina_stopwatch_duration(data->w, &elapsed_sec));
-    printf("Time for filling X and Y matrices: %.3g s, %.1f MB/s\n",
-        elapsed_sec, NELEM_BYTES(data->size_x + data->size_y) / (elapsed_sec * _IARRAY_SIZE_MB));
 
     iarray_dtshape_t xdtshape;
     xdtshape.ndim = 2;
     xdtshape.dtype = IARRAY_DATA_TYPE_DOUBLE;
     
     xdtshape.shape[0] = data->shape_x_0;
-    xdtshape.pshape[1] = data->pshape_x_1;
-    xdtshape.shape[0] = data->shape_x_0;
+    xdtshape.shape[1] = data->shape_x_1;
+    xdtshape.pshape[0] = data->pshape_x_0;
     xdtshape.pshape[1] = data->pshape_x_1;
     
     iarray_dtshape_t ydtshape;
     ydtshape.ndim = 2;
     ydtshape.dtype = IARRAY_DATA_TYPE_DOUBLE;
+
     ydtshape.shape[0] = data->shape_y_0;
+    ydtshape.shape[1] = data->shape_y_1;
+    ydtshape.pshape[0] = data->pshape_y_0;
     ydtshape.pshape[1] = data->pshape_y_1;
-    ydtshape.shape[0] = data->shape_y_0;
-    ydtshape.pshape[1] = data->pshape_y_1;
 
-    INA_STOPWATCH_START(data->w);
-    INA_MUST_SUCCEED(iarray_from_buffer(data->ctx, &xdtshape, data->mat_x, data->size_x, &data->mat_x_prop, data->flags, &data->con_x));
-    INA_MUST_SUCCEED(iarray_from_buffer(data->ctx, &ydtshape, data->mat_y, data->size_y, &data->mat_y_prop, data->flags, &data->con_y));
-    INA_STOPWATCH_STOP(data->w);
-    INA_MUST_SUCCEED(ina_stopwatch_duration(data->w, &elapsed_sec));
-
-    iarray_container_info(data->con_x, &data->nbytes, &data->cbytes);
-    INA_BENCH_MSG("Time for filling X and Y iarray-containers: %.3g s, %.1f MB/s",
-        elapsed_sec, NELEM_BYTES(data->size_x + data->size_y) / (elapsed_sec * _IARRAY_SIZE_MB));
-    data->nbytes_mb = ((double)data->nbytes / _IARRAY_SIZE_MB);
-    data->cbytes_mb = ((double)data->cbytes / _IARRAY_SIZE_MB);
-    INA_BENCH_MSG("Compression for X iarray-container: %.1f MB -> %.1f MB (%.1fx)",
-        data->nbytes_mb, data->cbytes_mb, ((double)data->nbytes / data->cbytes));
-
-    if (allocated == 0) {
-        INA_MUST_SUCCEED(iarray_to_buffer(data->ctx, data->con_x, data->mat_x, NELEM_BYTES(data->size_x)));
-        INA_MUST_SUCCEED(iarray_to_buffer(data->ctx, data->con_y, data->mat_y, NELEM_BYTES(data->size_y)));
-    }
-
-    data->mat_out = (double*)ina_mem_alloc((sizeof(double) * data->size_out));
-    data->mat_res = (double*)ina_mem_alloc((sizeof(double) * data->size_out));
-
+    INA_MUST_SUCCEED(iarray_from_buffer(data->ctx, &xdtshape, data->mat_x, data->size_x, NULL, data->flags, &data->con_x));
+    INA_MUST_SUCCEED(iarray_from_buffer(data->ctx, &ydtshape, data->mat_y, data->size_y, NULL, data->flags, &data->con_y));
+  
     return INA_SUCCESS;
 }
 
@@ -214,8 +179,6 @@ static ina_rc_t _teardown_matrices(struct matmul_data *data)
 
     ina_mem_free(data->mat_x);
     ina_mem_free(data->mat_y);
-    ina_mem_free(data->mat_out);
-    ina_mem_free(data->mat_res);
 
     return INA_SUCCESS;
 }
@@ -224,8 +187,6 @@ INA_BENCH_SETUP(matmul)
 {
     ina_bench_set_scale_label("matrix_result_size");
     ina_bench_set_precision(0);
-
-    INA_STOPWATCH_NEW(-1, -1, &data->w);
 
     INA_MUST_SUCCEED(iarray_init());
 
@@ -240,8 +201,7 @@ INA_BENCH_SETUP(matmul)
 
 INA_BENCH_TEARDOWN(matmul)
 {
-    // FIXME: compare
-    INA_STOPWATCH_FREE(&data->w);
+    INA_UNUSED(data);
     iarray_destroy();
 }
 
@@ -252,29 +212,25 @@ INA_BENCH_BEGIN(matmul, native_mkl)
 }
 
 INA_BENCH_SCALE(matmul) {
-    data->shape_x_0 = data->shape_x_0 + (100 * ina_bench_get_iteration());
+    //data->shape_x_0 = data->shape_x_0 + (100 * ina_bench_get_iteration());
     ina_bench_set_scale(data->size_out);
 }
 
 INA_BENCH(matmul, native_mkl, 2)
 {
-    double elapsed_sec = 0;
-
     _init_message(data);
 
-    _setup_matrices(data, 0);
+    _setup_matrices(data);
+
+    double *mat_res = (double*)ina_mem_alloc((sizeof(double) * data->size_out));
     
     /* Compute MKL matrix-matrix multiplication */
-    INA_STOPWATCH_START(data->w);
     ina_bench_stopwatch_start();
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (int)data->shape_x_0, (int)data->shape_y_1, (int)data->shape_x_1,
-        1.0, data->mat_x, (int)data->shape_x_1, data->mat_y, (int)data->shape_y_1, 0.0, data->mat_res, (int)data->shape_y_1);
+        1.0, data->mat_x, (int)data->shape_x_1, data->mat_y, (int)data->shape_y_1, 0.0, mat_res, (int)data->shape_y_1);
     ina_bench_set_int64(ina_bench_stopwatch_stop());
-    INA_STOPWATCH_STOP(data->w);
-    INA_MUST_SUCCEED(ina_stopwatch_duration(data->w, &elapsed_sec));
 
-    INA_BENCH_MSG("Time for multiplying two matrices (pure C, MKL): %.3g s, %.1f GFLOPs\n",
-        elapsed_sec, data->flops / (elapsed_sec * 10e9));
+    ina_mem_free(mat_res);
 }
 
 INA_BENCH_END(matmul, native_mkl)
@@ -290,42 +246,28 @@ INA_BENCH_BEGIN(matmul, linalg_matmul)
 
 INA_BENCH(matmul, linalg_matmul, 2)
 {
-    double elapsed_sec = 0;
-
     _init_message(data);
 
-    _setup_matrices(data, 0);
+    _setup_matrices(data);
 
     iarray_dtshape_t outdtshape;
     outdtshape.ndim = 2;
     outdtshape.dtype = IARRAY_DATA_TYPE_DOUBLE;
     
     outdtshape.shape[0] = data->shape_out_0;
-    outdtshape.pshape[1] = data->pshape_out_1;
-    outdtshape.shape[0] = data->shape_out_0;
+    outdtshape.shape[1] = data->shape_out_1;
+    outdtshape.pshape[0] = data->pshape_out_0;
     outdtshape.pshape[1] = data->pshape_out_1;
 
     uint64_t bshape_x[] = { data->bshape_x_0, data->bshape_x_1 };
     uint64_t bshape_y[] = { data->bshape_y_0, data->bshape_y_1 };
 
     iarray_container_t *con_out;
-    iarray_container_new(data->ctx, &outdtshape, &data->mat_out_prop, 0, &con_out);
+    iarray_container_new(data->ctx, &outdtshape, NULL, 0, &con_out);
 
-    INA_STOPWATCH_START(data->w);
     ina_bench_stopwatch_start();
-    iarray_linalg_matmul(data->ctx, data->con_x, data->con_y, con_out, bshape_x, bshape_y, IARRAY_OPERATOR_GENERAL); /* FIXME: error handling */
+    INA_MUST_SUCCEED(iarray_linalg_matmul(data->ctx, data->con_x, data->con_y, con_out, bshape_x, bshape_y, IARRAY_OPERATOR_GENERAL));
     ina_bench_set_int64(ina_bench_stopwatch_stop());
-    INA_STOPWATCH_STOP(data->w);
-    INA_MUST_SUCCEED(ina_stopwatch_duration(data->w, &elapsed_sec));
-
-    iarray_container_info(con_out, &data->nbytes, &data->cbytes);
-    INA_BENCH_MSG("Time for multiplying two matrices (iarray):  %.3g s, %.1f GFLOPs",
-        elapsed_sec, data->flops / (elapsed_sec * 10e9));
-
-    data->nbytes_mb = ((double)data->nbytes / _IARRAY_SIZE_MB);
-    data->cbytes_mb = ((double)data->cbytes / _IARRAY_SIZE_MB);
-    INA_BENCH_MSG("Compression for OUT values: %.1f MB -> %.1f MB (%.1fx)",
-        data->nbytes_mb, data->cbytes_mb, (1.*data->nbytes) / data->cbytes);
 }
 
 INA_BENCH_END(matmul, linalg_matmul)
