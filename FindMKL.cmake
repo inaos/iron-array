4469619c4273abf304bc219d82a461b6cfb53ea3
Christian Steiner <christian.steiner@inaos.ch>
2019-02-09 17:02:19 +0100

    workaround, bug with static linking on linux with mkl

--- FindMKL.cmake
+++ FindMKL.cmake
@@ -47,7 +47,7 @@ elseif(APPLE)
     set(MKL_LIBS libmkl_intel_lp64.a libmkl_core.a libmkl_sequential.a)
 else() # Linux
     set(MKL_SEARCH_LIB libmkl_core.a)
-    set(MKL_LIBS libmkl_intel_lp64.a libmkl_sequential.a libmkl_core.a)
+    set(MKL_LIBS mkl_intel_lp64 mkl_sequential mkl_core)
 endif()
 
 
