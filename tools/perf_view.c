6c85286737ca2f8c782d822d02fabe151cb4014d
aleix11alcacer <al312972@uji.es>
2019-02-18 10:02:02 +0100

    linalg_matmul works with views

--- tools/perf_view.c
+++ tools/perf_view.c
@@ -16,7 +16,7 @@
 int main(int argc, char **argv)
 {
     ina_stopwatch_t *w;
-    double elapsed_slice, elapsed_view;
+    double elapsed, elapsed_view;
     INA_STOPWATCH_NEW(-1, -1, &w);
 
     uint64_t shape_x[] = {10, 10, 10};
@@ -25,6 +25,10 @@ int main(int argc, char **argv)
 
     uint64_t pshape_y[] = {2, 1, 2};
     uint64_t pshape_z[] = {2, 1, 2};
+
+    uint64_t shape_mul[] = {4, 4};
+    uint64_t pshape_mul[] = {2, 2};
+    uint8_t ndim_mul = 2;
 
     int64_t start[] = {1, 3, 3};
     int64_t stop[] = {5, 4, 7};
@@ -58,8 +62,8 @@ int main(int argc, char **argv)
     INA_MUST_SUCCEED(iarray_squeeze(ctx, c_y));
     INA_STOPWATCH_STOP(w);
 
-    INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed_slice));
-    printf("Time for get_slice: %f\n", elapsed_slice);
+    INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed));
+    printf("Time for get_slice: %f\n", elapsed);
 
     INA_STOPWATCH_START(w);
     iarray_container_t *c_z;
@@ -70,9 +74,7 @@ int main(int argc, char **argv)
     INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed_view));
     printf("Time for get_slice (view): %f\n", elapsed_view);
 
-    printf("Speed-up: %f\n", elapsed_slice / elapsed_view);
-
-    INA_STOPWATCH_FREE(&w);
+    printf("Speed-up: %f\n", elapsed / elapsed_view);
 
     iarray_iter_read_block_t *iter_y;
     iarray_iter_read_block_t *iter_z;
@@ -102,12 +104,74 @@ int main(int argc, char **argv)
 
     iarray_iter_read_block_free(iter_y);
     iarray_iter_read_block_free(iter_z);
-    
+
+    iarray_dtshape_t dtshape_mul;
+
+    dtshape_mul.dtype = IARRAY_DATA_TYPE_DOUBLE;
+    dtshape_mul.ndim = ndim_mul;
+    for (int i = 0; i < dtshape_mul.ndim; ++i) {
+        dtshape_mul.shape[i] = shape_mul[i];
+        dtshape_mul.pshape[i] = pshape_mul[i];
+    }
+
+    iarray_container_t *c_mul;
+    iarray_container_t *c_mul_view;
+
+    INA_MUST_SUCCEED(iarray_container_new(ctx, &dtshape_mul, NULL, 0, &c_mul));
+    INA_MUST_SUCCEED(iarray_container_new(ctx, &dtshape_mul, NULL, 0, &c_mul_view));
+
+    INA_STOPWATCH_START(w);
+    INA_MUST_SUCCEED(iarray_linalg_matmul(ctx, c_y, c_y, c_mul, bshape, bshape, IARRAY_OPERATOR_GENERAL));
+    INA_STOPWATCH_STOP(w);
+    INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed));
+    printf("Time for matrix-matrix mult: %f\n", elapsed);
+
+    INA_STOPWATCH_START(w);
+    INA_MUST_SUCCEED(iarray_linalg_matmul(ctx, c_z, c_z, c_mul_view, bshape, bshape, IARRAY_OPERATOR_GENERAL));
+    INA_STOPWATCH_STOP(w);
+    INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed_view));
+    printf("Time for matrix-matrix mult (view): %f\n", elapsed_view);
+
+    printf("Speed-up: %f\n", elapsed / elapsed_view);
+
+    iarray_iter_read_block_t *iter_mul;
+    iarray_iter_read_block_t *iter_mul_view;
+
+    iarray_iter_read_block_new(ctx, c_mul, &iter_mul, bshape);
+    iarray_iter_read_block_new(ctx, c_mul_view, &iter_mul_view, bshape);
+
+    for (iarray_iter_read_block_init(iter_mul),
+             iarray_iter_read_block_init(iter_mul_view);
+         !iarray_iter_read_block_finished(iter_mul);
+         iarray_iter_read_block_next(iter_mul),
+             iarray_iter_read_block_next(iter_mul_view)) {
+        iarray_iter_read_block_value_t value_mul;
+        iarray_iter_read_block_value(iter_mul, &value_mul);
+        iarray_iter_read_block_value_t value_mul_view;
+        iarray_iter_read_block_value(iter_mul_view, &value_mul_view);
+
+        uint64_t bsize = 1;
+        for (int i = 0; i < c_mul->dtshape->ndim; ++i) {
+            bsize *= value_mul.block_shape[i];
+        }
+
+        for (int i = 0; i < bsize; ++i) {
+            INA_TEST_ASSERT_EQUAL_FLOATING(((double *) value_mul.pointer)[i], ((double *) value_mul_view.pointer)[i]);
+        }
+    }
+
+    iarray_iter_read_block_free(iter_mul);
+    iarray_iter_read_block_free(iter_mul_view);
+
     iarray_container_free(ctx, &c_x);
     iarray_container_free(ctx, &c_y);
     iarray_container_free(ctx, &c_z);
+    iarray_container_free(ctx, &c_mul);
+    iarray_container_free(ctx, &c_mul_view);
 
     iarray_context_free(&ctx);
+
+    INA_STOPWATCH_FREE(&w);
 
     return 0;
 }
