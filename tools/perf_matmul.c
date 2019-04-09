3c79306e1ccff536abff2f4864229f262c5cf3eb
Francesc Alted <faltet@gmail.com>
2019-04-09 10:27:09 +0200

    Remove unnecessary warning macros for MSVC

--- tools/perf_matmul.c
+++ tools/perf_matmul.c
@@ -63,10 +63,8 @@ int main(int argc, char** argv)
     int64_t pshape_y[] = {300, 478};
     int64_t bshape_y[] = {400, 600};
 
-    INA_DISABLE_WARNING_MSVC(4204)
     int64_t shape_out[] = {shape_x[0], shape_y[1]};
     int64_t pshape_out[] = {bshape_x[0], bshape_y[1]};
-    INA_ENABLE_WARNING_MSVC(4204)
 
     int64_t size_y = shape_y[0] * shape_y[1];
     int64_t size_out = shape_out[0] * shape_out[1];
@@ -98,11 +96,9 @@ int main(int argc, char** argv)
         printf("Storage for iarray matrices: *memory*\n");
     }
 
-    INA_DISABLE_WARNING_MSVC(4204)
     iarray_store_properties_t mat_x_prop = { .id = mat_x_name };
     iarray_store_properties_t mat_y_prop = { .id = mat_y_name };
     iarray_store_properties_t mat_out_prop = { .id = mat_out_name };
-    INA_ENABLE_WARNING_MSVC(4204)
 
     printf("\n");
     printf("Measuring time for multiplying matrices X and Y\n");
