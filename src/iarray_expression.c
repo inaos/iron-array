f5ca2ab9b1e03045b41b1cb059991e2131a3c553
aleix11alcacer <al312972@uji.es>
2019-05-13 10:55:26 +0200

    progress

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -306,6 +306,7 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
 #endif
 #if defined(_OPENMP)
 #pragma omp master
+            {
 #endif
             iarray_iter_write_block_next(iter_out);
             printf("Chunk %lld (thread %d)\n", out_value.nblock, nthread_);
@@ -359,12 +360,11 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
             // Write the resulting chunk in output
             nitems_written += out_items;
             ina_mempool_reset(e->ctx->mp_tmp_out);
-            #if defined(_OPENMP)
-            #pragma omp master
-            #endif
+
             has_next = iarray_iter_write_block_has_next(iter_out);
             printf("has_next %d\n", has_next);
 #if defined(_OPENMP)
+            }
 #pragma omp barrier
 #endif
         }
