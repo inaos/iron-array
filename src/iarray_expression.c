433a172992e70130a21ef49c3e9efc373f69376d
aleix11alcacer <al312972@uji.es>
2019-05-13 11:16:50 +0200

    progress

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -350,6 +350,7 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
             }
 
 #if defined(_OPENMP)
+#pragma omp barrier
 #pragma omp master
             {
 #endif
