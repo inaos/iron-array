aab9e30054d99dd434647685b9d2f67128902ab1
aleix11alcacer <al312972@uji.es>
2019-05-13 10:07:16 +0200

    progress

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -321,8 +321,7 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
             // Eval the expression for this chunk, split by blocks
             int nthread__ = 0;
 #if defined(_OPENMP)
-            #pragma omp for nowait schedule(dynamic)
-#endif
+            #endif
             for (int nblock = 0; nblock < nblocks; nblock++) {
 #if defined(_OPENMP)
 nthread__ = omp_get_thread_num();
