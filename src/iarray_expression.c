82913ecbc4cd6b69f447c992143732145ffa4fb8
aleix11alcacer <al312972@uji.es>
2019-05-13 10:14:36 +0200

    progress

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -316,15 +316,14 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
                 iarray_iter_read_block_next(iter_var[nvar]);
             }
 #if defined(_OPENMP)
-            }
 #endif
             // Eval the expression for this chunk, split by blocks
             int nthread__ = 0;
 #if defined(_OPENMP)
-            #endif
+#endif
             for (int nblock = 0; nblock < nblocks; nblock++) {
 #if defined(_OPENMP)
-nthread__ = omp_get_thread_num();
+                nthread__ = omp_get_thread_num();
 #endif
                 printf("- Block %d (thread %d)\n", nblock, nthread__);
                 for (int nvar = 0; nvar < nvars; nvar++) {
@@ -341,8 +340,7 @@ nthread__ = omp_get_thread_num();
             }
 
 #if defined(_OPENMP)
-            #pragma omp single
-            {
+
 #endif
             // Do a possible last evaluation with the leftovers
             int leftover = out_items * e->typesize - nblocks * blocksize;
