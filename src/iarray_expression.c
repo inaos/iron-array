6d57f17af50eafaca0961f6e7e1ce3d321312168
aleix11alcacer <al312972@uji.es>
2019-05-13 11:11:30 +0200

    progress

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -329,7 +329,7 @@ INA_API(ina_rc_t) iarray_eval(iarray_expression_t *e, iarray_container_t *ret)
             int nthread__ = 0;
 
 #if defined(_OPENMP)
-#pragma omp for nowait
+#pragma omp for
 #endif
             for (int nblock = 0; nblock < nblocks; nblock++) {
 #if defined(_OPENMP)
