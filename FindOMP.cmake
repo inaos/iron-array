b1c8006451d4eee3fd772f2f97c2e25a5877d384
aleix11alcacer <al312972@uji.es>
2019-05-24 10:46:22 +0200

    Add FindOMP.cmake (Intel version)

---
+++ FindOMP.cmake
@@ -0,0 +1,64 @@
+# Find the Intel MKL (Math Kernel Library)
+#
+# MKL_FOUND - System has MKL
+# MKL_INCLUDE_DIRS - MKL include files directories
+# MKL_LIBRARIES - The MKL libraries
+#
+# The environment variable MKLROOT is used to find the installation location.
+# If the environment variable is not set we'll look for it in the default installation locations.
+#
+# Usage:
+#
+# find_package(MKL)
+# if(MKL_FOUND)
+#     target_link_libraries(TARGET ${MKL_LIBRARIES})
+# endif()
+
+# Currently we take a couple of assumptions:
+#
+# 1. We only use the sequential version of the MKL
+# 2. We only use 64bit
+#
+
+find_path(OMP_ROOT_DIR
+    lib/libiomp5.a
+    PATHS
+        $ENV{OMPROOT}
+        /opt/intel/compilers_and_libraries/linux
+        /opt/intel/compilers_and_libraries/mac
+        "C:/IntelSWTools/compilers_and_libraries/windows"
+        "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows"
+        $ENV{HOME}/miniconda3
+        $ENV{USERPROFILE}/miniconda3/Library
+        "C:/Miniconda37-x64/Library" # Making AppVeyor happy
+        $ENV{CONDA}/Library # Azure pipeline hosted windows agent
+)
+
+
+if(APPLE)
+    set(OMP_SEARCH_LIB libiomp5.a)
+    set(OMP_LIBS libiomp5.a)
+else() # Linux
+    set(OMP_SEARCH_LIB libiomp5.a)
+    set(OMP_LIBS libiomp5.a)
+endif()
+
+
+find_path(OMP_LIB_SEARCHPATH
+    ${OMP_SEARCH_LIB}
+    PATHS
+        ${OMP_ROOT_DIR}/lib
+)
+
+foreach (LIB ${OMP_LIBS})
+    find_library(${LIB}_PATH ${LIB} PATHS ${OMP_LIB_SEARCHPATH})
+    if(${LIB}_PATH)
+        set(OMP_LIBRARIES ${OMP_LIBRARIES} ${${LIB}_PATH})
+        message(STATUS "Found OMP ${LIB} in: ${${LIB}_PATH}")
+    else()
+        message(STATUS "Could not find ${LIB}: disabling OMP")
+    endif()
+endforeach()
+
+
+set(INAC_DEPENDENCY_LIBS ${INAC_DEPENDENCY_LIBS} ${OMP_LIBRARIES})
