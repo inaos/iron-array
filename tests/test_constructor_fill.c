54286be8c8d1c97e076fc990f8734891e3ddac08
aleix11alcacer <al312972@uji.es>
2019-10-18 10:44:23 +0200

    Modify fill test

--- tests/test_constructor_fill.c
+++ tests/test_constructor_fill.c
@@ -125,7 +125,7 @@ INA_TEST_FIXTURE(constructor_fill, 7_f_p)
 
     int8_t ndim = 7;
     int64_t shape[] = {12, 11, 6, 5, 12, 6, 8};
-    int64_t pshape[] = {13, 3, 10, 3, 3, 10};
+    int64_t pshape[] = {13, 3, 10, 3, 3, 5, 10};
     float value = -116.f;
 
     INA_TEST_ASSERT_SUCCEED(test_fill(data->ctx, dtype, type_size, ndim, shape, pshape, &value));
