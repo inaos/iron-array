7312d282d9209bfab7bd9817902f123e855463b9
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-02-17 09:28:13 +0100

    Improve the API of some functions (#266)
    * Improve load function API
    
    * Improve API
    
    * Fix examples
    
    * Fix load_save bug
    
    * Fix load_save bug

--- tests/test_view_serialization.c
+++ tests/test_view_serialization.c
@@ -17,7 +17,7 @@
 static ina_rc_t test_slice(iarray_context_t *ctx, iarray_container_t *c_x, int64_t *start,
                            int64_t *stop, const int64_t *pshape, iarray_store_properties_t *stores,
                            int flags, iarray_container_t **c_out) {
-    INA_TEST_ASSERT_SUCCEED(iarray_get_slice(ctx, c_x, start, stop, pshape, stores, flags, true, c_out));
+    INA_TEST_ASSERT_SUCCEED(iarray_get_slice(ctx, c_x, start, stop, true, pshape, stores, flags, c_out));
     INA_TEST_ASSERT_SUCCEED(iarray_squeeze(ctx, *c_out));
 
     return INA_SUCCESS;
