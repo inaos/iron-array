0e99b9fa79f46fb3ce5a023b86738dece7836ea6
Christian Steiner <christian.steiner@inaos.ch>
2020-03-13 10:44:59 +0100

    trying to fix svml detection

--- FindSVML.cmake
+++ FindSVML.cmake
@@ -18,6 +18,7 @@ find_path(SVML_ROOT_DIR
         $ENV{HOME}/miniconda3/lib
         $ENV{USERPROFILE}/miniconda3/Library
         $ENV{CONDA}/envs/iArrayEnv/lib/intel64 # Azure pipelines
+		$ENV{CONDA}/envs/iArrayEnv/lib # Azure pipelines
         /Users/vsts/.conda/envs/iArrayEnv # Azure pipelines
         C:/Miniconda/envs/iArrayEnv # Azure pipelines
 )
