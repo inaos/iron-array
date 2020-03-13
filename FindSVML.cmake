a5ae8587b8f31c839a8538600cb93a76d085e11d
Francesc Alted <faltet@gmail.com>
2020-03-13 10:49:41 +0100

    Much simplified version for finding SVML using ctypes

--- FindSVML.cmake
+++ FindSVML.cmake
@@ -2,35 +2,18 @@
 #
 # Usage:
 # find_package(SVML)
+#
+# Note: this will emit a FATAL_ERROR in case the SVML library is not found (i.e. hard dependency)
 
-if(APPLE)
-    set(SVML_LIB libsvml.dylib)
-elseif(WIN32)
-    set(SVML_LIB svml_dispmd.dll)
+# Use the fantastic ctypes shared library mechanism for finding *any* library on the system
+EXEC_PROGRAM("python" ARGS "-c 'from ctypes.util import find_library; print(find_library(\"svml\"))'"
+             OUTPUT_VARIABLE SVML_LIB RETURN_VALUE RET_VAL)
+
+if(${RET_VAL} EQUAL 0 AND EXISTS ${SVML_LIB})
+    set(SVML_LIBRARY ${SVML_LIB})
+    message(STATUS "Found SVML lib iat: ${SVML_LIBRARY}")
 else()
-    set(SVML_LIB libsvml.so)
+    message(FATAL_ERROR "Could not find SVML lib")
 endif()
-
-find_path(SVML_ROOT_DIR
-    ${SVML_LIB}
-    PATHS
-        $ENV{SVMLROOT}
-        $ENV{HOME}/miniconda3/lib
-        $ENV{USERPROFILE}/miniconda3/Library
-        $ENV{CONDA}/envs/iArrayEnv/lib/intel64 # Azure pipelines
-        /Users/vsts/.conda/envs/iArrayEnv # Azure pipelines
-        C:/Miniconda/envs/iArrayEnv # Azure pipelines
-)
-
-foreach (LIB ${SVML_LIB})
-    message(STATUS "Looking ${LIB} in: ${${LIB}_PATH}")
-    find_library(${LIB}_PATH ${LIB} PATHS ${SVML_ROOT_DIR})
-    if(${LIB}_PATH)
-        set(SVML_LIBRARY ${SVML_LIBRARY} ${${LIB}_PATH})
-        message(STATUS "Found SVML ${LIB} in: ${${LIB}_PATH}")
-    else()
-        message(FATAL_ERROR "Could not find ${LIB}")
-    endif()
-endforeach()
 
 set(INAC_DEPENDENCY_LIBS ${INAC_DEPENDENCY_LIBS} ${SVML_LIBRARY})
