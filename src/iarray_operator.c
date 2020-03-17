203cacf2329466d4b00d8c249427fccde348cabc
aleix11alcacer <al312972@uji.es>
2020-03-17 16:43:13 +0100

    Remove no copy slice

--- src/iarray_operator.c
+++ src/iarray_operator.c
@@ -173,25 +173,28 @@ static ina_rc_t _iarray_gemm(iarray_context_t *ctx, iarray_container_t *a, iarra
         }
 
         // Obtain desired blocks from iarray containers
-        if (!a->view && a->catarr->storage == CATERVA_STORAGE_PLAINBUFFER && a_contiguous) {
-            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer_no_copy(ctx, a, start_a, stop_a, (void **) &a_block, a_size));
-        } else {
-            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, a, start_a, stop_a, bshape_a, a_block, a_size));
-        }
+//        if (!a->view && a->catarr->storage == CATERVA_STORAGE_PLAINBUFFER && a_contiguous) {
+//            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer_no_copy(ctx, a, start_a, stop_a, (void **) &a_block, a_size));
+//        } else {
+//            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, a, start_a, stop_a, bshape_a, a_block, a_size));
+//        }
+//        if (!b->view && b->catarr->storage == CATERVA_STORAGE_PLAINBUFFER && b_contiguous) {
+//            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer_no_copy(ctx, b, start_b, stop_b, (void **) &b_block, b_size));
+//        } else {
+//            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, b, start_b, stop_b, bshape_b, b_block, b_size));
+//        }
 
-        if (!b->view && b->catarr->storage == CATERVA_STORAGE_PLAINBUFFER && b_contiguous) {
-            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer_no_copy(ctx, b, start_b, stop_b, (void **) &b_block, b_size));
-        } else {
-            IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, b, start_b, stop_b, bshape_b, b_block, b_size));
-        }
+        IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, a, start_a, stop_a, bshape_a, a_block, a_size));
+        IARRAY_FAIL_IF_ERROR(_iarray_get_slice_buffer(ctx, b, start_b, stop_b, bshape_b, b_block, b_size));
 
         // Make blocks multiplication
-        mkl_set_num_threads(ctx->cfg->max_num_threads);
+
+        // mkl_set_num_threads(ctx->cfg->max_num_threads);
 
         switch (dtype) {
             case IARRAY_DATA_TYPE_DOUBLE:
                 cblas_dgemm(CblasRowMajor, flag_a, flag_b, (int) B0, (int) B2, (int) B1, 1.0, (double *)a_block, ld_a, (double *)b_block, ld_b, 1.0, (double *)c_block, ld_c);
-                //mult_c((double *) a_block, (double *) b_block, (double *) c_block, B0, B2, B1);
+                // mult_c((double *) a_block, (double *) b_block, (double *) c_block, B0, B2, B1);
 
                 break;
             case IARRAY_DATA_TYPE_FLOAT:
@@ -229,12 +232,17 @@ static ina_rc_t _iarray_gemm(iarray_context_t *ctx, iarray_container_t *a, iarra
     rc = ina_err_get_rc();
     cleanup:
     _iarray_iter_matmul_free(&iter);
-    if (a->view || a->catarr->storage == CATERVA_STORAGE_BLOSC || !a_contiguous) {
-        INA_MEM_FREE_SAFE(a_block);
-    }
-    if (b->view || b->catarr->storage == CATERVA_STORAGE_BLOSC || !b_contiguous) {
-        INA_MEM_FREE_SAFE(b_block);
-    }
+
+    // if (a->view || a->catarr->storage == CATERVA_STORAGE_BLOSC || !a_contiguous) {
+    //    INA_MEM_FREE_SAFE(a_block);
+    // }
+    // if (b->view || b->catarr->storage == CATERVA_STORAGE_BLOSC || !b_contiguous) {
+    //    INA_MEM_FREE_SAFE(b_block);
+    // }
+
+    INA_MEM_FREE_SAFE(a_block);
+    INA_MEM_FREE_SAFE(b_block);
+
     if (c->catarr->storage != CATERVA_STORAGE_PLAINBUFFER) {
         INA_MEM_FREE_SAFE(c_block);
     }
