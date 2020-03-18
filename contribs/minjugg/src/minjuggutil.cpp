62c47f617c1af0fbd771d927bee220165751a9fe
aleix11alcacer <al312972@uji.es>
2020-03-18 13:40:10 +0100

    Activate SVML

--- contribs/minjugg/src/minjuggutil.cpp
+++ contribs/minjugg/src/minjuggutil.cpp
@@ -17,7 +17,7 @@ extern "C" int jug_util_set_svml_vector_library()
     const char *argv[2];
     argv[0] = "opt";
     argv[1] = "-vector-library=SVML";
-    llvm::cl::ParseCommandLineOptions(1, argv);
+    llvm::cl::ParseCommandLineOptions(2, argv);
     return 0;
 }
 
