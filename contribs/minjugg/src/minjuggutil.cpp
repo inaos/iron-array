03fbb043d0d9e950ae63bbaeed6a9cdad491fa5f
Francesc Alted <faltet@gmail.com>
2020-04-02 10:27:05 +0200

    New DISABLE_SVML envvar for disabling SVML.  Fixes #294.

--- contribs/minjugg/src/minjuggutil.cpp
+++ contribs/minjugg/src/minjuggutil.cpp
@@ -1,4 +1,5 @@
 #include "minjuggutil.h"
+#include <stdlib.h>
 
 #include <llvm-c/Transforms/PassManagerBuilder.h>
 
@@ -17,7 +18,9 @@ extern "C" int jug_util_set_svml_vector_library()
     const char *argv[2];
     argv[0] = "opt";
     argv[1] = "-vector-library=SVML";
-    llvm::cl::ParseCommandLineOptions(2, argv);
+    char *envvar = getenv("DISABLE_SVML");  // useful for debugging purposes
+    int nargs = (envvar != NULL) ? 1 : 2;
+    llvm::cl::ParseCommandLineOptions(nargs, argv);
     return 0;
 }
 
