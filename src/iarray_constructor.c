9ae8d88e2830fddffeaf284dd4c424e1364b3740
aleix11alcacer <al312972@uji.es>
2020-04-07 13:10:49 +0200

    Fix some bugs

--- src/iarray_constructor.c
+++ src/iarray_constructor.c
@@ -384,9 +384,26 @@ INA_API(ina_rc_t) iarray_from_buffer(iarray_context_t *ctx,
     caterva_context_t *cat_ctx;
     IARRAY_ERR_CATERVA(caterva_context_new(&cfg, &cat_ctx));
 
+    uint8_t *smeta = NULL;
+    if (storage.backend == CATERVA_STORAGE_BLOSC) {
+        blosc2_metalayer *metalayer = (*container)->catarr->sc->metalayers[1];
+        storage.properties.blosc.nmetalayers = 1;
+        storage.properties.blosc.metalayers[0].name = "iarray";
+        uint32_t smeta_len = metalayer->content_len;
+        smeta = malloc(smeta_len * 2);
+        blosc2_get_metalayer((*container)->catarr->sc, "iarray", &smeta, &smeta_len);
+        storage.properties.blosc.metalayers[0].sdata = smeta;
+        storage.properties.blosc.metalayers[0].size = smeta_len;
+    }
     IARRAY_ERR_CATERVA(caterva_array_free(cat_ctx, &(*container)->catarr));
 
+    printf("%s\n", storage.properties.blosc.metalayers[0].name);
     IARRAY_ERR_CATERVA(caterva_array_from_buffer(cat_ctx, buffer, buflen, &params, &storage, &(*container)->catarr));
+
+    if (storage.backend == CATERVA_STORAGE_BLOSC) {
+        free(smeta);
+    }
+    (*container)->catarr->empty = false;
 
     IARRAY_ERR_CATERVA(caterva_context_free(&cat_ctx));
 
