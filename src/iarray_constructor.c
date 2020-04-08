878e1ce6da6db8c96ed2f267ee41cb1a76e65750
aleix11alcacer <al312972@uji.es>
2020-04-08 11:01:30 +0200

    Remove * 2

--- src/iarray_constructor.c
+++ src/iarray_constructor.c
@@ -390,7 +390,7 @@ INA_API(ina_rc_t) iarray_from_buffer(iarray_context_t *ctx,
         storage.properties.blosc.nmetalayers = 1;
         storage.properties.blosc.metalayers[0].name = "iarray";
         uint32_t smeta_len = metalayer->content_len;
-        smeta = malloc(smeta_len * 2);
+        smeta = malloc(smeta_len);
         blosc2_get_metalayer((*container)->catarr->sc, "iarray", &smeta, &smeta_len);
         storage.properties.blosc.metalayers[0].sdata = smeta;
         storage.properties.blosc.metalayers[0].size = smeta_len;
