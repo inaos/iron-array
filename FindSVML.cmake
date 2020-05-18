2014e0ba8a71930429ddbffe3cfd78bac9d7edae
Francesc Alted <faltet@gmail.com>
2020-05-18 20:46:36 +0200

    Prune one more path for SVML

--- FindSVML.cmake
+++ FindSVML.cmake
@@ -17,7 +17,6 @@ find_path(SVML_ROOT_DIR
         $ENV{SVMLROOT}
         $ENV{CONDA_PREFIX}/lib  # conda environments are accessible here (including base)
         $ENV{CONDA}/envs/iArrayEnv/lib  # not sure why this would be needed (old conda on azure?)
-        $ENV{CONDA}/envs/iArrayEnv  # Win (same: old conda on azure?)
         $ENV{CONDA}/envs/iArrayEnv/Library/bin  # Win (very weird to me)
         /opt/intel/compilers_and_libraries/linux/lib/intel64_lin  # Intel ICC on Linux
 	    /opt/intel/compilers_and_libraries/mac/lib/intel64_lin  # Intel ICC on MacOS
