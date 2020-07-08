903de2bdf849a983843a1cb6829c9f2b44b54d61
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-07-08 12:28:12 +0200

    WIP

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -523,6 +523,10 @@ int prefilter_func(blosc2_prefilter_params *pparams)
         }
         else {
             eval_pparams.inputs[i] = expr_pparams->inputs[i] + pparams->out_offset;
+            for (int j = 0; j < bsize / typesize; j++) {
+                printf(" %f ", ((double *) eval_pparams.inputs[i])[j]);
+            }
+            printf("\n");
         }
     }
 
@@ -728,6 +732,8 @@ INA_API(ina_rc_t) iarray_eval_iterblosc(iarray_expression_t *e, iarray_container
             if (INA_FAILED(iarray_iter_read_block_next(iter_var[nvar], NULL, 0))) {
                 goto fail;
             }
+            memset((int8_t *) external_buffers[nvar], 0,
+                              ret->catarr->extchunknitems * ret->catarr->itemsize);
             caterva_blosc_array_repart_chunk((int8_t *) external_buffers[nvar],
                               ret->catarr->extchunknitems * ret->catarr->itemsize,
                                              iter_value[nvar].block_pointer,
