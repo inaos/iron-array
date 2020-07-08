3dbefde72bea9fb53c9b9f8092eac7a2f540a09b
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-07-08 11:44:04 +0200

    WIP

--- src/iarray_expression.c
+++ src/iarray_expression.c
@@ -417,14 +417,14 @@ int prefilter_func(blosc2_prefilter_params *pparams)
     int32_t strides[IARRAY_DIMENSION_MAX];
     strides[ndim - 1] = 1;
     for (int i = ndim - 2; i >= 0 ; --i) {
-        strides[i] = strides[i+1] * e->out->catarr->blockshape[i];
+        strides[i] = strides[i+1] * e->out->catarr->blockshape[i+1];
     }
 
     // Block strides (in blocks)
     int32_t strides_block[IARRAY_DIMENSION_MAX];
     strides_block[ndim - 1] = 1;
     for (int i = ndim - 2; i >= 0 ; --i) {
-        strides_block[i] = strides_block[i+1] * e->out->catarr->extchunkshape[i] / e->out->catarr->blockshape[i];
+        strides_block[i] = strides_block[i+1] * e->out->catarr->extchunkshape[i+1] / e->out->catarr->blockshape[i+1];
     }
 
     // Flattened block number
