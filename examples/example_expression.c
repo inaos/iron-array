6f91bfcfc3665fbef4e8a8d02588278503e98597
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-08-07 13:04:02 +0200

    WIP

--- examples/example_expression.c
+++ examples/example_expression.c
@@ -13,26 +13,31 @@
 #include <libiarray/iarray.h>
 #include <math.h>
 
-int main(void)
-{
+
+double fexpr(double x, double y, double z) {
+    return sin(x) * sin(y) + cos(z);
+}
+
+
+int main(void) {
     iarray_init();
     ina_stopwatch_t *w;
 
     double elapsed_sec = 0;
     INA_STOPWATCH_NEW(-1, -1, &w);
 
-    char *expr = "2*x+y";
+    char *expr = "sin(x) * sin(y) + cos(z)";
 
     iarray_context_t *ctx;
     iarray_config_t cfg = IARRAY_CONFIG_DEFAULTS;
-    cfg.compression_level = 0;
-    cfg.eval_flags = IARRAY_EVAL_METHOD_AUTO;
+    cfg.compression_level = 9;
+    cfg.eval_flags = IARRAY_EVAL_METHOD_ITERBLOSC;
     cfg.max_num_threads = 4;
     iarray_context_new(&cfg, &ctx);
 
 
-    int64_t shape[] = {4000, 4000};
-    int8_t ndim = 2;
+    int64_t shape[] = {20 * 1000 * 1000};
+    int8_t ndim = 1;
     int8_t typesize = sizeof(double);
 
     iarray_dtshape_t dtshape;
@@ -45,8 +50,8 @@ int main(void)
         nelem *= shape[i];
     }
 
-    int32_t xchunkshape[] = {400, 800};
-    int32_t xblockshape[] = {100, 150};
+    int32_t xchunkshape[] = {4000 * 1000};
+    int32_t xblockshape[] = {32 * 1000};
     iarray_storage_t xstorage;
     xstorage.backend = IARRAY_STORAGE_BLOSC;
     xstorage.enforce_frame = false;
@@ -56,8 +61,8 @@ int main(void)
         xstorage.blockshape[i] = xblockshape[i];
     }
 
-    int32_t ychunkshape[] = {400, 800};
-    int32_t yblockshape[] = {100, 150};
+    int32_t ychunkshape[] = {4000 * 1000};
+    int32_t yblockshape[] = {32 * 1000};
     iarray_storage_t ystorage;
     ystorage.backend = IARRAY_STORAGE_BLOSC;
     ystorage.enforce_frame = false;
@@ -67,8 +72,19 @@ int main(void)
         ystorage.blockshape[i] = yblockshape[i];
     }
 
-    int32_t outchunkshape[] = {400, 800};
-    int32_t outblockshape[] = {100, 150};
+    int32_t zchunkshape[] = {4000 * 1000};
+    int32_t zblockshape[] = {32 * 1000};
+    iarray_storage_t zstorage;
+    zstorage.backend = IARRAY_STORAGE_BLOSC;
+    zstorage.enforce_frame = false;
+    zstorage.filename = NULL;
+    for (int i = 0; i < ndim; ++i) {
+        zstorage.chunkshape[i] = zchunkshape[i];
+        zstorage.blockshape[i] = zblockshape[i];
+    }
+
+    int32_t outchunkshape[] = {4000 * 1000};
+    int32_t outblockshape[] = {32 * 1000};
     iarray_storage_t outstorage;
     outstorage.backend = IARRAY_STORAGE_BLOSC;
     outstorage.enforce_frame = false;
@@ -77,46 +93,57 @@ int main(void)
         outstorage.chunkshape[i] = outchunkshape[i];
         outstorage.blockshape[i] = outblockshape[i];
     }
-    
-    iarray_container_t* c_x;
-    iarray_container_t* c_y;
-    iarray_arange(ctx, &dtshape, 0, nelem, 1, &xstorage, 0, &c_x);
-    iarray_arange(ctx, &dtshape, 0, nelem, 1, &ystorage, 0, &c_y);
 
-    iarray_expression_t* e;
+    iarray_container_t *c_x;
+    iarray_container_t *c_y;
+    iarray_container_t *c_z;
+
+    iarray_linspace(ctx, &dtshape, nelem, 0, 1, &xstorage, 0, &c_x);
+    iarray_linspace(ctx, &dtshape, nelem, 0, 1, &ystorage, 0, &c_y);
+    iarray_linspace(ctx, &dtshape, nelem, 0, 1, &zstorage, 0, &c_z);
+
+    iarray_expression_t *e;
     iarray_expr_new(ctx, &e);
     iarray_expr_bind(e, "x", c_x);
     iarray_expr_bind(e, "y", c_y);
+    iarray_expr_bind(e, "z", c_z);
     iarray_expr_bind_out_properties(e, &dtshape, &outstorage);
 
     iarray_expr_compile(e, expr);
 
 
-    iarray_container_t* c_out;
+    iarray_container_t *c_out;
 
+    int nrep = 5;
     INA_STOPWATCH_START(w);
-    IARRAY_RETURN_IF_FAILED(iarray_eval(e, &c_out));
+    for (int i = 0; i < nrep; ++i) {
+        IARRAY_RETURN_IF_FAILED(iarray_eval(e, &c_out));
+    }
     INA_STOPWATCH_STOP(w);
     INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed_sec));
 
     int64_t nbytes = nelem * typesize;
 
     printf("Time for eval expression: %.3g s, %.1f GB/s\n",
-           elapsed_sec, nbytes / (elapsed_sec * (1u << 20u)));
+           elapsed_sec, nbytes * nrep / (elapsed_sec * (1u << 20u)));
 
     uint64_t b_size = nelem * typesize;
     uint8_t *b_x = ina_mem_alloc(b_size);
     uint8_t *b_y = ina_mem_alloc(b_size);
+    uint8_t *b_z = ina_mem_alloc(b_size);
     uint8_t *b_out = ina_mem_alloc(b_size);
 
     IARRAY_RETURN_IF_FAILED(iarray_to_buffer(ctx, c_x, b_x, b_size));
     IARRAY_RETURN_IF_FAILED(iarray_to_buffer(ctx, c_y, b_y, b_size));
+    IARRAY_RETURN_IF_FAILED(iarray_to_buffer(ctx, c_z, b_z, b_size));
     IARRAY_RETURN_IF_FAILED(iarray_to_buffer(ctx, c_out, b_out, b_size));
 
     for (int i = 1; i < nelem; ++i) {
         double d1 = ((double *) b_out)[i];
-        double d2 = 2 * ((double *) b_x)[i] + ((double *) b_y)[i];
-        
+        double d2 = fexpr(((double *) b_x)[i],
+                          ((double *) b_y)[i],
+                          ((double *) b_z)[i]);
+
         double rerr = fabs((d1 - d2) / d1);
         if (rerr > 1e-15) {
             printf("ERROR at [%d]!\n", i);
@@ -132,6 +159,7 @@ int main(void)
 
     INA_MEM_FREE_SAFE(b_x);
     INA_MEM_FREE_SAFE(b_y);
+    INA_MEM_FREE_SAFE(b_z);
     INA_MEM_FREE_SAFE(b_out);
 
     INA_STOPWATCH_FREE(&w);
