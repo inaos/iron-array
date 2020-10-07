87f02cdc4d2acdd98b398666370eaf294cdbeaa1
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-10-07 13:29:38 +0200

    Parallelize matmul algorithm (#357)
    * Parallelize matmul algorithm
    
    * Fix examples and benchmarks
    
    * Remove unused lines
    
    * Replace file headers

---
+++ src/linalg/matmul/gemm.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright INAOS GmbH, Thalwil, 2018.
+ * Copyright Francesc Alted, 2018.
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of INAOS GmbH
+ * and Francesc Alted ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the license agreement.
+ *
+ */
+
+#ifndef IARRAY_GEMM_H
+#define IARRAY_GEMM_H
+
+INA_API(ina_rc_t) iarray_gemm(iarray_context_t *ctx,
+                              iarray_container_t *a,
+                              iarray_container_t *b,
+                              iarray_container_t *c);
+
+#endif //IARRAY_GEMM_H
--- tools/perf_matmul.c
+++ tools/perf_matmul.c
@@ -244,10 +244,9 @@ int main(int argc, char** argv)
     }
 
     iarray_container_t *con_out;
-    iarray_container_new(ctx, &outdtshape, &mat_out_prop, 0, &con_out);
 
     INA_STOPWATCH_START(w);
-    iarray_linalg_matmul(ctx, con_x, con_y, con_out, blockshape_x, blockshape_y, IARRAY_OPERATOR_GENERAL); /* FIXME: error handling */
+    IARRAY_RETURN_IF_FAILED(iarray_linalg_matmul(ctx, con_x, con_y, &mat_out_prop, &con_out));
     INA_STOPWATCH_STOP(w);
     INA_MUST_SUCCEED(ina_stopwatch_duration(w, &elapsed_sec));
 
