900865d01ed861baeb36e98e8b5ebaa43e50c6a5
Francesc Alted <francesc@blosc.org>
2021-09-10 13:48:41 +0200

    Add a new program for detecting possible leaks in gemv

--- bench/bench_gemv.c
+++ bench/bench_gemv.c
@@ -11,7 +11,6 @@
  */
 
 #include <libiarray/iarray.h>
-#include <mkl.h>
 
 
 int main(void)
@@ -28,20 +27,20 @@ int main(void)
     int8_t ndim_y = 1;
     int8_t ndim_z = 1;
 
-    int64_t shape_x[] = {2000, 2000};
-    int64_t shape_y[] = {2000};
-    int64_t shape_z[] = {2000};
+    int64_t shape_x[] = {1000000, 140};
+    int64_t shape_y[] = {140};
+    int64_t shape_z[] = {1000000};
 
-    int64_t size_x = shape_x[0] * shape_x[1];
-    int64_t size_y = shape_y[0];
-    int64_t size_z = shape_z[0];
+//    int64_t size_x = shape_x[0] * shape_x[1];
+//    int64_t size_y = shape_y[0];
+//    int64_t size_z = shape_z[0];
 
-    int64_t cshape_x[] = {1000, 1000};
-    int64_t cshape_y[] = {1000};
-    int64_t cshape_z[] = {1000};
+    //int64_t cshape_x[] = {5000, 140};
+    int64_t cshape_y[] = {140};
+    int64_t cshape_z[] = {5000};
 
-    int64_t bshape_x[] = {200, 200};
-    int64_t bshape_y[] = {200};
+    //int64_t bshape_x[] = {200, 140};
+    int64_t bshape_y[] = {140};
     int64_t bshape_z[] = {200};
 
     iarray_config_t cfg = IARRAY_CONFIG_DEFAULTS;
@@ -60,16 +59,19 @@ int main(void)
         dtshape_x.shape[i] = shape_x[i];
     }
 
-    iarray_storage_t store_x;
-    store_x.backend = IARRAY_STORAGE_BLOSC;
-    store_x.enforce_frame = false;
-    store_x.urlpath = NULL;
-    for (int i = 0; i < ndim_x; ++i) {
-        store_x.chunkshape[i] = cshape_x[i];
-        store_x.blockshape[i] = bshape_x[i];
-    }
+//    iarray_storage_t store_x;
+//    store_x.backend = IARRAY_STORAGE_BLOSC;
+//    store_x.enforce_frame = false;
+//    store_x.urlpath = "arr.iarr";
+//    for (int i = 0; i < ndim_x; ++i) {
+//        store_x.chunkshape[i] = cshape_x[i];
+//        store_x.blockshape[i] = bshape_x[i];
+//    }
+//    IARRAY_FAIL_IF_ERROR(iarray_linspace(ctx, &dtshape_x, 3, 2, &store_x, 0, &c_x));
     iarray_container_t *c_x;
-    IARRAY_FAIL_IF_ERROR(iarray_linspace(ctx, &dtshape_x, 3, 2, &store_x, 0, &c_x));
+    int niter = 100;
+    //IARRAY_FAIL_IF_ERROR(iarray_container_load(ctx, "../examples/arr.iarr", &c_x));
+    IARRAY_FAIL_IF_ERROR(iarray_container_open(ctx, "../examples/arr.iarr", &c_x));
 
     iarray_dtshape_t dtshape_y;
     dtshape_y.ndim = ndim_y;
@@ -104,53 +106,21 @@ int main(void)
     }
     iarray_container_t *c_z;
 
-    mkl_set_num_threads(n_threads);
-
-    double *b_x = (double *) malloc(size_x * sizeof(double));
-    double *b_y = (double *) malloc(size_y * sizeof(double));
-    double *b_z = (double *) malloc(size_z * sizeof(double));
-    double *b_res = (double *) malloc(size_z * sizeof(double));
-
-    IARRAY_FAIL_IF_ERROR(iarray_to_buffer(ctx, c_x, b_x, size_x * sizeof(double)));
-    IARRAY_FAIL_IF_ERROR(iarray_to_buffer(ctx, c_y, b_y, size_y * sizeof(double)));
-
     INA_STOPWATCH_START(w);
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, (int) shape_x[0], (int) shape_x[1],
-                1.0, b_x, (int) shape_x[1], b_y, 1, 0.0, b_z, 1);
-    INA_STOPWATCH_STOP(w);
-    IARRAY_FAIL_IF_ERROR(ina_stopwatch_duration(w, &elapsed_sec));
-
-    printf("Time mkl (C): %.4f\n", elapsed_sec);
-
-    INA_STOPWATCH_START(w);
-    if (INA_FAILED(iarray_opt_gemv(ctx, c_x, c_y, false, &store_z, &c_z))) {
-        fprintf(stderr, "Error in linalg_gemv1: %s\n", ina_err_strerror(ina_err_get_rc()));
-        goto fail;
+    for (int j = 0; j < niter; j++) {
+        if (INA_FAILED(iarray_opt_gemv(ctx, c_x, c_y, false, &store_z, &c_z))) {
+            fprintf(stderr, "Error in linalg_gemv1: %s\n", ina_err_strerror(ina_err_get_rc()));
+            goto fail;
+        }
     }
     INA_STOPWATCH_STOP(w);
     IARRAY_FAIL_IF_ERROR(ina_stopwatch_duration(w, &elapsed_sec));
 
     printf("Time iarray gemv: %.4f\n", elapsed_sec);
 
-    free(b_x);
-    free(b_y);
     iarray_container_free(ctx, &c_x);
     iarray_container_free(ctx, &c_y);
-
-    IARRAY_FAIL_IF_ERROR(iarray_to_buffer(ctx, c_z, b_res, size_z * sizeof(double)));
-
-    for (int64_t i = 0; i < size_z; ++i) {
-        if (fabs((b_res[i] - b_z[i]) / b_res[i]) > 1e-8) {
-            fprintf(stderr, "%f - %f = %f\n", b_res[i], b_z[i], b_res[i] - b_z[i]);
-            fprintf(stderr, "Error in element %" PRId64 "\n", i);
-            return INA_ERROR(INA_ERR_ERROR);
-        }
-    }
-
-
     iarray_container_free(ctx, &c_z);
-    free(b_z);
-    free(b_res);
     iarray_context_free(&ctx);
 
     INA_STOPWATCH_FREE(&w);
